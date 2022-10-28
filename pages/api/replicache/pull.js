1739bc84645d6e55a2739c8c9b71a2ccfaa24ed1
Martin Adams <martin@consciousapps.com>
2022-10-28 07:50:06 -0700

    Fix: interactive transactions error - “Transaction API error: Transaction not found”

--- pages/api/replicache/pull.js
+++ pages/api/replicache/pull.js
@@ -17,39 +17,48 @@ const PagesApiReplicachePull = async (req, res) => {
 	if (!clientID || !spaceId || cookie === undefined) return res.status(403)
 
 	try {
-		await prisma.$transaction(async tx => {
-			// #1. Get last mutation Id for the current client
-			let { data: lastMutationId } = await utilApiLastMutationIdGet({ clientID, tx })
+		await prisma.$transaction(
+			async tx => {
+				// #1. Get last mutation Id for the current client
+				let { data: lastMutationId } = await utilApiLastMutationIdGet({ clientID, tx })
 
-			// #2. Get all transactions done after the last client request for the current space
-			const { data: apiEntriesTodoGet } = await utilApiEntriesTodoGet({
-				cookie,
-				spaceId,
-				tx,
-				userId: user.id
-			})
+				// #2. Get all transactions done after the last client request for the current space
+				const { data: apiEntriesTodoGet } = await utilApiEntriesTodoGet({
+					cookie,
+					spaceId,
+					tx,
+					userId: user.id
+				})
 
-			// #3. Get the highest authoritative version number
-			const replicacheVersion = apiEntriesTodoGet?.length
-				? Math.max(...apiEntriesTodoGet?.map(x => x.lastModifiedVersion))
-				: 0
+				// #3. Get the highest authoritative version number
+				const replicacheVersion = apiEntriesTodoGet?.length
+					? Math.max(...apiEntriesTodoGet?.map(x => x.lastModifiedVersion))
+					: 0
 
-			// #4. Put together a patch with instructions for the client
-			const patch = []
+				// #4. Put together a patch with instructions for the client
+				const patch = []
 
-			if (cookie === null) patch.push({ op: 'clear' })
+				if (cookie === null) patch.push({ op: 'clear' })
 
-			patch.push(
-				...apiEntriesTodoGet?.map(todo => ({
-					op: !todo.isDeleted ? 'put' : 'del',
-					key: `todo/${todo.todoId}`,
-					value: { ...todo }
-				}))
-			)
+				patch.push(
+					...apiEntriesTodoGet?.map(todo => ({
+						op: !todo.isDeleted ? 'put' : 'del',
+						key: `todo/${todo.todoId}`,
+						value: { ...todo }
+					}))
+				)
 
-			// #5. Return object to client
-			res.json({ lastMutationID: lastMutationId, cookie: replicacheVersion, patch })
-		})
+				// #5. Return object to client
+				res.json({ lastMutationID: lastMutationId, cookie: replicacheVersion, patch })
+
+				return true
+			},
+			// FIX: interactive transactions error still occurs with long wait times
+			{
+				maxWait: 20000,
+				timeout: 60000
+			}
+		)
 	} catch (err) {
 		console.error(err)
 
