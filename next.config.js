d0494820381a8739fa103cb776930cc14cb995bd
Martin Adams <martin@consciousapps.com>
2022-10-28 09:05:45 -0700

    [dx] Use PlanetScale test database - publish test secrets - remove Doppler

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	env: {
 		CLERK_API_KEY: process.env.CLERK_API_KEY,
 		CLERK_JWT_KEY: process.env.CLERK_JWT_KEY,
+		DATABASE_URL: process.env.DATABASE_URL,
 		ENV: process.env.ENV, // `dev`, `stg`, or `prd`
 		PUSHER_REPLICACHE: process.env.PUSHER_REPLICACHE
 	},
