1268dfadf3cb86e72c781309a674e88b3f9866f5
Martin Adams <martin@consciousapps.com>
2022-12-16 09:16:08 -0800

    [refactor] - remove Clerk authentication - replace with basic cookie auth - update Prisma v4.7.1

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // Packages
 import { getCookie } from 'cookies-next'
 
-const UtilsAuth = async (req, _) => {
+const UtilsAuth = async (req, res) => {
 	// In this demo, we’re just using basic cookies and not implementing a secure authentication system since auth isn’t the purpose of this demo. In a production app you’d implement a secure authentication system.
 	const user = getCookie('user', { req, res })
 
